20167165c94a2bc0dd418e14818e278ea8ea13a8
buying.wang <buying.wang@tinklabs.com>
2019-06-07 11:37:38 +0800

    lint error

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -1,13 +1,20 @@
 <?php
+/**
+ * This controller is used to say something out.
+ */
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
+    /**
+     * Say specified words or default say world.
+     */
     public function say(Request $request)
     {
         $word = $request->get('word', 'world');
         return "Hello $word";
     }
+
 }
