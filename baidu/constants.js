ea26dc2619896c0af068b125104f2b4fcfb6f35b
wbylovesun <wbylovesun@users.noreply.github.com>
2020-05-10 21:37:41 +0800

    Create constants.js

---
+++ baidu/constants.js
@@ -0,0 +1,27 @@
+const BCE_HOST = "https://aip.baidubce.com"
+
+const BCE_APIS = {
+    OAUTH: "/oauth/2.0/token",
+    FACE: {
+        LIVENESS: {
+            SESSION_CODE: "/rest/2.0/face/v1/faceliveness/sessioncode",
+            VERIFY: "/reset/2.0/face/v1/faceliveness/verify"
+        },
+        PERSON: {
+            VERIFY: "/rest/2.0/face/v3/person/verify"
+        }
+    }
+}
+
+const DEFAULTS = {
+    SESSION_CODE: {
+        MIN_CODE_LENGTH: 3,
+        MAX_CODE_LENGTH: 6
+    }
+}
+
+export default {
+    BCE_HOST,
+    BCE_APIS,
+    DEFAULTS,
+}
